5ab712edc9edc9ea9348f0d56276bbc3c2a73dd6
Dennis Codes <dennis@ngeni.io>
2022-11-03 19:39:45 +0300

    getting started on rust

---
+++ guessing_game/src/main.rs
@@ -0,0 +1,30 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
+fn main() {
+    println!("guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    loop {
+        let mut guess = String::new();
+        println!("please input your guess.");
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("failed to read line");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        println!("you guessed: {guess}");
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("too small!"),
+            Ordering::Greater => println!("too big!"),
+            Ordering::Equal => {
+                println!("you win!");
+                break;
+            }
+        }
+    }
+}
