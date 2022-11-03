3dc41750fe45518370b76e850674a022be80d259
Dennis Codes <dennis@ngeni.io>
2022-11-03 19:45:59 +0300

    working with imojis

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -8,21 +8,21 @@ fn main() {
     let secret_number = rand::thread_rng().gen_range(1..=100);
     loop {
         let mut guess = String::new();
-        println!("please input your guess.");
+        println!("please input your guess.🤔");
 
         io::stdin()
             .read_line(&mut guess)
-            .expect("failed to read line");
+            .expect("failed to read line❌");
         let guess: u32 = match guess.trim().parse() {
             Ok(num) => num,
             Err(_) => continue,
         };
         println!("you guessed: {guess}");
         match guess.cmp(&secret_number) {
-            Ordering::Less => println!("too small!"),
-            Ordering::Greater => println!("too big!"),
+            Ordering::Less => println!("too small!👎"),
+            Ordering::Greater => println!("too big!👎"),
             Ordering::Equal => {
-                println!("you win!");
+                println!("you win!✅");
                 break;
             }
         }
